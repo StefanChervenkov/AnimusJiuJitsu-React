41e5e26aedaf55249f1aef83f26c8452cc55719c
unknown <stefan.chervenkov.sth@gmail.com>
2023-11-03 16:18:19 +0200

    Minor styles changes

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -2,12 +2,15 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function StudentCard() {
+function StudentCard(props) {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card  
+        style={{ width: '18rem' }}
+        bg='light'
+        >
             <Card.Img variant="top" src="https://www.ukbjja.org/wp-content/uploads/2021/07/cecrphk0fnushkq5ix0o.jpg" />
             <Card.Body>
-                <Card.Title>Name</Card.Title>
+                <Card.Title>{props.name}</Card.Title>
                 <ListGroup variant="flush">
                     <ListGroup.Item>Blue Belt </ListGroup.Item>
                     <ListGroup.Item>Adult</ListGroup.Item>
--- src/pages/AllStudents.jsx
+++ src/pages/AllStudents.jsx
@@ -3,12 +3,12 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export default function AllStudents() {
-
+    const studentName = 'Sara Johnsson'
     return (
         <Row xs={1} md={4} className="g-4">
             {Array.from({ length: 10 }).map((_, idx) => (
                 <Col key={idx}>
-                    <StudentCard/>
+                    <StudentCard name={studentName} />
                 </Col>
             ))}
         </Row>
