5ab98cccd540138e971946e5e75f05be775bdd4e
unknown <stefan.chervenkov.sth@gmail.com>
2023-11-22 15:49:50 +0200

    implemented fetching student data from the db and visuialize teh data in the profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,34 @@
 import { auth, database } from "../firebase";
-import { set, ref } from "firebase/database";
+import { set, ref, onValue } from "firebase/database";
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { ListGroup } from "react-bootstrap";
 
-const studentInfo = {
-    username: 'Stefan',
-    email: 'testemail@gmail.com',
-    profile_picture: 'someImageUrl'
-}
+import { useState, useEffect } from "react";
 
 
 export default function Profile() {
     const user = auth.currentUser;
+    const [studentName, setStudentName] = useState('')
+
+
+    useEffect(() => {
+        const studentRef = ref(database, 'students/' + user.uid);
+
+        const onDataChange = (snapshot) => {
+            const data = snapshot.val();
+            setStudentName(data.username);
+        };
+
+        onValue(studentRef, onDataChange);
+
+        return () => {
+            // This will unsubscribe the listener when the component is unmounted
+            onValue(studentRef, onDataChange);
+        };
+
+    }, [user.uid]);
 
 
     // function writeStudentData() {
@@ -32,8 +47,8 @@ export default function Profile() {
                 style={{ maxWidth: '50%', maxHeight: '300px', marginLeft: '150px' }}
             />
             <Card.Body>
-                <Card.Title>Stefan Chervenkov</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">stefan.chervenkov.sth@gmail.com</Card.Subtitle>
+                <Card.Title>{studentName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{user.email}</Card.Subtitle>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
