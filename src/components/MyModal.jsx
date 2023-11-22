71c72ab2eefa591e3ff511972342491b3bf6fe36
unknown <stefan.chervenkov.sth@gmail.com>
2023-11-22 16:55:57 +0200

    added a modal which shows when the student profile is empty and promts the student to complete the profile

---
+++ src/components/MyModal.jsx
@@ -0,0 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function MyModal(props) {
+  return (
+    <Modal
+      
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          {props.text}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,17 @@
 import { auth, database } from "../firebase";
 import { set, ref, onValue } from "firebase/database";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { ListGroup } from "react-bootstrap";
+import { Button, Modal, ListGroup, Card } from 'react-bootstrap';
+
 
 import { useState, useEffect } from "react";
 
 
 export default function Profile() {
     const user = auth.currentUser;
-    const [studentName, setStudentName] = useState('')
+    const [studentName, setStudentName] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const handleClose = () => setShowModal(false);
 
 
     useEffect(() => {
@@ -18,7 +19,14 @@ export default function Profile() {
 
         const onDataChange = (snapshot) => {
             const data = snapshot.val();
-            setStudentName(data.username);
+            if (data == null) {
+                setShowModal(true);
+
+
+            } else {
+                setStudentName(data.username);
+            }
+
         };
 
         onValue(studentRef, onDataChange);
@@ -26,44 +34,64 @@ export default function Profile() {
         return () => {
             // This will unsubscribe the listener when the component is unmounted
             onValue(studentRef, onDataChange);
+
         };
 
     }, [user.uid]);
 
 
-    // function writeStudentData() {
-    //     console.log(user.uid);
-    //     set(ref(database, 'students/' + user.uid), studentInfo);
-
-    // }
-
 
     return (
-        <Card style={{ width: '50rem', marginTop: '20px', marginLeft: '20px' }}>
-            <Card.Img
-                variant="top"
+        <>
 
-                src="https://flo-static-assets.s3.amazonaws.com/uploads/api/5835abd50880e.jpeg"
-                style={{ maxWidth: '50%', maxHeight: '300px', marginLeft: '150px' }}
-            />
-            <Card.Body>
-                <Card.Title>{studentName}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{user.email}</Card.Subtitle>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <ListGroup className="list-group-flush" style={{ marginBottom: '20px' }}>
-                    <ListGroup.Item>Belt Level: Blue Belt</ListGroup.Item>
-                    <ListGroup.Item>Age: 30</ListGroup.Item>
-                    <ListGroup.Item>Favorite technique: Armbar</ListGroup.Item>
-                </ListGroup>
-                <Button style={{ marginLeft: '20px' }} variant="primary">Edit Profile</Button>
-                <Button style={{ marginLeft: '20px' }} variant="primary">Payments</Button>
-                <Button style={{ marginLeft: '20px' }} variant="primary">Trainings</Button>
+            <Card style={{ width: '50rem', marginTop: '20px', marginLeft: '20px' }}>
+                <Card.Img
+                    variant="top"
+
+                    src="https://flo-static-assets.s3.amazonaws.com/uploads/api/5835abd50880e.jpeg"
+                    style={{ maxWidth: '50%', maxHeight: '300px', marginLeft: '150px' }}
+                />
+                <Card.Body>
+                    <Card.Title>{studentName}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{user.email}</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <ListGroup className="list-group-flush" style={{ marginBottom: '20px' }}>
+                        <ListGroup.Item>Belt Level: ......</ListGroup.Item>
+                        <ListGroup.Item>Age: .....</ListGroup.Item>
+                        <ListGroup.Item>Favorite technique: .....</ListGroup.Item>
+                    </ListGroup>
+                    <Button style={{ marginLeft: '20px' }} variant="primary">Edit Profile</Button>
+                    <Button style={{ marginLeft: '20px' }} variant="primary">Payments</Button>
+                    <Button style={{ marginLeft: '20px' }} variant="primary">Trainings</Button>
 
 
-            </Card.Body>
-        </Card>
+                </Card.Body>
+            </Card>
+
+           
+            {showModal && (
+                <>
+
+                    <Modal show={showModal} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Student Profile is empty</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Please take a moment to add some details, such as your profile picture, age, and favorite techniques. Your complete profile ensures you get the most out of our platform. Thank you! 🥋✨"</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+
+                        </Modal.Footer>
+                    </Modal>
+                </>
+            )}
+
+        </>
+
+
     );
 }
